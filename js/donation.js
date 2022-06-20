a227775d9b60a65b58d6918934c3319df6d81855
Ashish <sam.kingston.96@gmail.com>
2022-06-20 23:58:24 +0530

    changes

--- js/donation.js
+++ js/donation.js
@@ -1,7 +1,7 @@
 
     setTimeout(function() {
         window.location.replace("Thankyou.html")
-    }, 40000)
+    }, 50000)
 
      let divForm=document.querySelector("h3");
       divForm.addEventListener("click",function(){
